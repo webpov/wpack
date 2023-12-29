f6fb189afde2d4521d53ceb7a753aed38cd4d536
Abraham Duno <abraham.d@servicepad.com>
2023-12-29 19:35:22 -0400

    fix links

--- src/model/level/pack/DEFAULT_PACKS.ts
+++ src/model/level/pack/DEFAULT_PACKS.ts
@@ -40,21 +40,21 @@ export const TIERPACK_NAMES = [
 ]
 
 export const TIERPACK_LINKS = [
-    `/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&c0=%7B%22symbol%22%3A%22RUNEUSDT%22%7D&b1=%7B%22symbol%22%3A%22LINKUSDT%22%7D&d0=%7B%22symbol%22%3A%22SOLUSDT%22%7D&b1=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&d1=%7B%22symbol%22%3A%22FILUSDT%22%7D&e0=%7B%22symbol%22%3A%22GRTUSDT%22%7D&e1=%7B%22symbol%22%3A%22RNDRUSDT%22%7D&e2=%7B%22symbol%22%3A%22TIAUSDT%22%7D`, // top10
-    `/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&b0=%7B%22symbol%22%3A%22ADAUSDT%22%7D&b1=%7B%22symbol%22%3A%22UNIUSDT%22%7D&c0=%7B%22symbol%22%3A%22AXSUSDT%22%7D&c1=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&d0=%7B%22symbol%22%3A%22SANDUSDT%22%7D&d1=%7B%22symbol%22%3A%22MKRUSDT%22%7D&e0=%7B%22symbol%22%3A%22FETUSDT%22%7D&e1=%7B%22symbol%22%3A%22RNDRUSDT%22%7D&e2=%7B%22symbol%22%3A%22TIAUSDT%22%7D`, // all\ndefi
-    `/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&b0=%7B%22symbol%22%3A%22ADAUSDT%22%7D`, // Layer 1 (L1)
-    `/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&b0=%7B%22symbol%22%3A%22XRPUSDT%22%7D&b1=%7B%22symbol%22%3A%22UNIUSDT%22%7D&c0=%7B%22symbol%22%3A%22ICPUSDT%22%7D&c1=%7B%22symbol%22%3A%22FILUSDT%22%7D&d0=%7B%22symbol%22%3A%22FLOWUSDT%22%7D&d1=%7B%22symbol%22%3A%22MKRUSDT%22%7D&e0=%7B%22symbol%22%3A%22ROSEUSDT%22%7D&e1=%7B%22symbol%22%3A%22SUIUSDT%22%7D&e2=%7B%22symbol%22%3A%22ARUSDT%22%7D`, // a16z
-    `/?a0=%7B%22symbol%22%3A%22XRPUSDT%22%7D&a1=%7B%22symbol%22%3A%22LTCUSDT%22%7D&b0=%7B%22symbol%22%3A%22DOTUSDT%22%7D`, // Smart Contract Platform
-    `/?a0=%7B%22symbol%22%3A%22LINKUSDT%22%7D&a1=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&b0=%7B%22symbol%22%3A%22COMPUSDT%22%7D`, // DeFi
-    `/?a0=%7B%22symbol%22%3A%22UNIUSDT%22%7D&a1=%7B%22symbol%22%3A%22SOLUSDT%22%7D&b0=%7B%22symbol%22%3A%22RUNEUSDT%22%7D`, // Decentralized Exchange (DEX)
-    `/?a0=%7B%22symbol%22%3A%22RNDRUSDT%22%7D&a1=%7B%22symbol%22%3A%22TIAUSDT%22%7D&b0=%7B%22symbol%22%3A%22FETUSDT%22%7D`, // AI
-    `/?a0=%7B%22symbol%22%3A%22BNBUSDT%22%7D&a1=%7B%22symbol%22%3A%22CAKEUSDT%22%7D&b0=%7B%22symbol%22%3A%22UNIUSDT%22%7D`, // Liquidity
-    `/?a0=%7B%22symbol%22%3A%22DOGEUSDT%22%7D&a1=%7B%22symbol%22%3A%22SHIBUSDT%22%7D&b0=%7B%22symbol%22%3A%22AKITAUSDT%22%7D`, // Memes
-    `/?a0=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&a1=%7B%22symbol%22%3A%22UNIUSDT%22%7D&b0=%7B%22symbol%22%3A%22MKRUSDT%22%7D`, // Governance
-    `/?a0=%7B%22symbol%22%3A%22MATICUSDT%22%7D&a1=%7B%22symbol%22%3A%22DOTUSDT%22%7D&b0=%7B%22symbol%22%3A%22XDAIUSDT%22%7D`, // Layer 2 (L2)
-    `/?a0=%7B%22symbol%22%3A%22MANAUSDT%22%7D&a1=%7B%22symbol%22%3A%22AXSUSDT%22%7D&b0=%7B%22symbol%22%3A%22SANDUSDT%22%7D`, // GameFi
-    `/?a0=%7B%22symbol%22%3A%22MANAUSDT%22%7D&a1=%7B%22symbol%22%3A%22AXSUSDT%22%7D&b0=%7B%22symbol%22%3A%22ENJUSDT%22%7D`, // Metaverse
-    `/?a0=%7B%22symbol%22%3A%22XMRUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETCUSDT%22%7D&b0=%7B%22symbol%22%3A%22ZECUSDT%22%7D`, // Privacy Coins
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&c0=%7B%22symbol%22%3A%22RUNEUSDT%22%7D&b1=%7B%22symbol%22%3A%22LINKUSDT%22%7D&d0=%7B%22symbol%22%3A%22SOLUSDT%22%7D&b1=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&d1=%7B%22symbol%22%3A%22FILUSDT%22%7D&e0=%7B%22symbol%22%3A%22GRTUSDT%22%7D&e1=%7B%22symbol%22%3A%22RNDRUSDT%22%7D&e2=%7B%22symbol%22%3A%22TIAUSDT%22%7D`, // top10
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&b0=%7B%22symbol%22%3A%22ADAUSDT%22%7D&b1=%7B%22symbol%22%3A%22UNIUSDT%22%7D&c0=%7B%22symbol%22%3A%22AXSUSDT%22%7D&c1=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&d0=%7B%22symbol%22%3A%22SANDUSDT%22%7D&d1=%7B%22symbol%22%3A%22MKRUSDT%22%7D&e0=%7B%22symbol%22%3A%22FETUSDT%22%7D&e1=%7B%22symbol%22%3A%22RNDRUSDT%22%7D&e2=%7B%22symbol%22%3A%22TIAUSDT%22%7D`, // all\ndefi
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&b0=%7B%22symbol%22%3A%22ADAUSDT%22%7D`, // Layer 1 (L1)
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22BTCUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETHUSDT%22%7D&b0=%7B%22symbol%22%3A%22XRPUSDT%22%7D&b1=%7B%22symbol%22%3A%22UNIUSDT%22%7D&c0=%7B%22symbol%22%3A%22ICPUSDT%22%7D&c1=%7B%22symbol%22%3A%22FILUSDT%22%7D&d0=%7B%22symbol%22%3A%22FLOWUSDT%22%7D&d1=%7B%22symbol%22%3A%22MKRUSDT%22%7D&e0=%7B%22symbol%22%3A%22ROSEUSDT%22%7D&e1=%7B%22symbol%22%3A%22SUIUSDT%22%7D&e2=%7B%22symbol%22%3A%22ARUSDT%22%7D`, // a16z
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22XRPUSDT%22%7D&a1=%7B%22symbol%22%3A%22LTCUSDT%22%7D&b0=%7B%22symbol%22%3A%22DOTUSDT%22%7D`, // Smart Contract Platform
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22LINKUSDT%22%7D&a1=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&b0=%7B%22symbol%22%3A%22COMPUSDT%22%7D`, // DeFi
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22UNIUSDT%22%7D&a1=%7B%22symbol%22%3A%22SOLUSDT%22%7D&b0=%7B%22symbol%22%3A%22RUNEUSDT%22%7D`, // Decentralized Exchange (DEX)
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22RNDRUSDT%22%7D&a1=%7B%22symbol%22%3A%22TIAUSDT%22%7D&b0=%7B%22symbol%22%3A%22FETUSDT%22%7D`, // AI
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22BNBUSDT%22%7D&a1=%7B%22symbol%22%3A%22CAKEUSDT%22%7D&b0=%7B%22symbol%22%3A%22UNIUSDT%22%7D`, // Liquidity
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22DOGEUSDT%22%7D&a1=%7B%22symbol%22%3A%22SHIBUSDT%22%7D&b0=%7B%22symbol%22%3A%22AKITAUSDT%22%7D`, // Memes
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22AAVEUSDT%22%7D&a1=%7B%22symbol%22%3A%22UNIUSDT%22%7D&b0=%7B%22symbol%22%3A%22MKRUSDT%22%7D`, // Governance
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22MATICUSDT%22%7D&a1=%7B%22symbol%22%3A%22DOTUSDT%22%7D&b0=%7B%22symbol%22%3A%22XDAIUSDT%22%7D`, // Layer 2 (L2)
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22MANAUSDT%22%7D&a1=%7B%22symbol%22%3A%22AXSUSDT%22%7D&b0=%7B%22symbol%22%3A%22SANDUSDT%22%7D`, // GameFi
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22MANAUSDT%22%7D&a1=%7B%22symbol%22%3A%22AXSUSDT%22%7D&b0=%7B%22symbol%22%3A%22ENJUSDT%22%7D`, // Metaverse
+    `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22XMRUSDT%22%7D&a1=%7B%22symbol%22%3A%22ETCUSDT%22%7D&b0=%7B%22symbol%22%3A%22ZECUSDT%22%7D`, // Privacy Coins
     `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22FILUSDT%22%7D&a1=%7B%22symbol%22%3A%22ARBUSDT%22%7D&b0=%7B%22symbol%22%3A%22STORJUSDT%22%7D`, // Storage
     `https://wtrade.vercel.app/?a0=%7B%22symbol%22%3A%22LINKUSDT%22%7D&a1=%7B%22symbol%22%3A%22GRTUSDT%22%7D&b0=%7B%22symbol%22%3A%22BANDUSDT%22%7D`, // Oracle
 ];
